43162672842bc3a5db07e4ae8313af4a73b97cda
Gamer 1 <65615643+gamer167@users.noreply.github.com>
2025-12-01 11:00:18 -0500

    DAY 1 - ADVENT OF CODE HAS STARTED!
    ayyyyyyy future me can you finish day 1?

---
+++ 2025/day1.js
@@ -0,0 +1,14 @@
+const currentDialLocation = 50
+const dialTurns = []
+
+for (let i = 0; i < dialTurns.length; i++) {
+
+  const cell = dialTurns[i]
+  if (cell.length > 0) {
+    if (cell.startsWith('R')) {
+      
+    } elseif (cell.startsWith('L')) {
+      
+    }
+  }
+}
