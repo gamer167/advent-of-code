2d2b13036344de8e4a48975095caddebb5ea7ec4
Gamer 1 <65615643+gamer167@users.noreply.github.com>
2025-12-05 08:45:44 -0500

    Create part1.py

---
+++ 2025/day4/part1.py
@@ -0,0 +1,31 @@
+"""
+Author: Jose A. Romero
+Edited by: Gamer 1.
+Puzzle: Advent of Code (year=2025 ; day=4 ; task=1)
+"""
+
+import sys
+
+def reader():
+    return open(f"imput.txt", 'r').read().splitlines()
+
+def main() -> None:
+    grid = reader()
+
+    n, m = len(grid), len(grid[0])
+    ans = 0
+
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] == "@":
+                cnt = 0
+                for di in range(-1, 2, 1):
+                    for dj in range(-1, 2, 1):
+                        cnt += (0 <= i + di < n) and (0 <= j + dj < m) and grid[i + di][j + dj] == "@"
+
+                ans += cnt < 5  # It should be less than 5 because when di = dj = 0 then it counts itself :)
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
